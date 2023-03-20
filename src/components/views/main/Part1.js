6f9db98025add1c10743fbceb6d993af0f156cb6
onzero98 <dev_210@naver.com>
2023-03-20 15:57:32 +0900

    MONKEY STOCK : title changed

--- src/components/views/main/Part1.js
+++ src/components/views/main/Part1.js
@@ -1,12 +1,12 @@
 import { useEffect, useRef } from 'react';
 
 function Part1() {
-
+  
   return (
     <section id="Part1">
-      <div>
-        <p> SKUCOIN </p>
-        <p> | 모의 코인 투자 웹 서비스 </p>
+      <div className='title'> MONKEY STOCK </div>
+      <div className='scroll-ui-wrap'>
+        <div className='txt'>scroll down</div>
       </div>
     </section>
   )
